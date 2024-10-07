622fdc9c17da27de9c0289285527b27409a64823
Parkavi <parkaviparkavi15@gmail.com>
2024-10-07 20:19:19 +0530

    Initial App setup

---
+++ NodeApp/config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const MAX_RETRIES = 5; // Set a limit for maximum retry attempts
+let retries = 0;
+
+const connectDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB Connected:', connect.connection.host);
+  } catch (err) {
+    retries++;
+    console.log(`Error connecting to MongoDB: ${err.message}. Retrying in 3 seconds... (Attempt ${retries}/${MAX_RETRIES})`);
+
+    if (retries < MAX_RETRIES) {
+      setTimeout(connectDb, 3000); // Retry connection after 3 seconds
+    } else {
+      console.log('Max retry attempts reached. Exiting...');
+      process.exit(1);  //terminate a Node.js process due to an Error, if it's 0 exit after succesful completion
+    }
+  }
+};
+
+module.exports = connectDb;
